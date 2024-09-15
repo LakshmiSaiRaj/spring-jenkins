93cd4f47bf1fec7f07ad7e96fccceeb76ea75d9f
Lakshmi Sai Raj <sairaj.n49@gmail.com>
2024-09-15 12:16:14 +0530

    Add Maven Wrapper configuration

--- src/main/java/com/example/demo4/Demo4Application.java
+++ src/main/java/com/example/demo4/Demo4Application.java
@@ -1,12 +1,22 @@
 package com.example.demo4;
 
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Demo4Application {
+	public static Logger logger = LoggerFactory.getLogger(Demo4Application.class);
+
+	@PostConstruct
+	public void init(){
+		logger.info("Application Started........");
+	}
 
 	public static void main(String[] args) {
+		logger.info("Application executed........");
 		SpringApplication.run(Demo4Application.class, args);
 	}
 
--- src/test/java/com/example/demo4/Demo4ApplicationTests.java
+++ src/test/java/com/example/demo4/Demo4ApplicationTests.java
@@ -1,13 +1,16 @@
 package com.example.demo4;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Demo4ApplicationTests {
-
+	public static Logger logger = LoggerFactory.getLogger(Demo4Application.class);
 	@Test
 	void contextLoads() {
+		logger.info("Test case executing...");
 	}
 
 }
